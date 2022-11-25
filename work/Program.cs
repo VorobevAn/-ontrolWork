7604bc5280441a426a0ce55b3ece5a553075591a
Vorodev <cust89@mail.ru>
2022-11-25 19:10:56 +0500

    second text array

--- work/Program.cs
+++ work/Program.cs
@@ -5,10 +5,23 @@
 
 //Console.WriteLine("Введите текст: ");
 //string t = Console.ReadLine()!;
-string[] text = {"Olya","has","a","pencil","Olya","has","a","pen","She","draws","with","a","pencil","She","writes","with","a","pen"};
+string[] text = { "Olya", "has", "a", "pencil", "Olya", "has", "a", "pen", "She", "draws", "with", "a", "pencil", "She", "writes", "with", "a", "pen" };
 
 for (int i = 0; i < text.Length; i++)
 {
     Console.Write($"{text[i]} ");
 }
+int j = 0;
+for (int i = 0; i < text.Length; i++)
+{
+    string[] text2 = new string[text.Length];
+    
+    if (text[i].Length <= 3)
+    {
+        text2[j] = text[i];
+        j++;
+    }
+}
 
+ 
+   
