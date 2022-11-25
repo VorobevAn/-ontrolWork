a7b67da5d2e4ac9119f2c663636bd805f325c23c
Vorodev <cust89@mail.ru>
2022-11-25 18:08:57 +0500

    text array

--- work/Program.cs
+++ work/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Сформировать из массива строк массив строк длиной до 3 символов
+//Первоночальный массив можно ввести с клавиатуры или задать на старте.
+
+
+//Console.WriteLine("Введите текст: ");
+//string t = Console.ReadLine()!;
+string[] text = {"Olya","has","a","pencil","Olya","has","a","pen","She","draws","with","a","pencil","She","writes","with","a","pen"};
+
+for (int i = 0; i < text.Length; i++)
+{
+    Console.Write($"{text[i]} ");
+}
+
